103b2b9531a4c050ec555431f0fab0f89affa301
KrishnanBabu <krishyukesh@gmail.com>
2019-08-01 17:44:24 +0530

    testing simply

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const register=require('./controllers/register');
 const signin=require('./controllers/signin');
 const profile=require('./controllers/profile');
 const image=require('./controllers/image');
-
+//ddfdfd
 const db=knex({
     client: 'pg',
     connection: {
