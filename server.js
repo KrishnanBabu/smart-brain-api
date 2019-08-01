a06854ddd83d4f1b8d37aaf930b8e26e05568bf9
KrishnanBabu <krishyukesh@gmail.com>
2019-08-01 17:17:11 +0530

    removing database const

--- server.js
+++ server.js
@@ -21,26 +21,7 @@ const db=knex({
 app.use(bodyParser.json());
 
 app.use(cors());
-const database={
-    users:[
-        {
-            id:'123',
-            name:'John',
-            email:'John@gmail.com',
-            password:'cookies',
-            entries:0,
-            joined:new Date()
-        },
-        {
-            id:'124',
-            name:'Sally',
-            email:'sally@gmail.com',
-            password:'bananas',
-            entries:0,
-            joined:new Date()
-        }
-    ]
-}
+
 
 app.get('/',(req,res)=>{res.send(database.users)})
 
