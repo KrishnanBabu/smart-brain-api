a6c90ac511567d04615f2c3423856ffc0b766945
KrishnanBabu <krishyukesh@gmail.com>
2019-08-01 18:26:50 +0530

    adding database connection

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image=require('./controllers/image');
 const db=knex({
     client: 'pg',
     connection: {
-      host : 'postgresql-infinite-98635',
-      user : 'postgres',
-      password : 'Krishitha@1',
-      database : 'smart-brain'
+      host : process.env.DATABASE_URL,
+      ssl:true,
     }
   });   
 app.use(bodyParser.json());
